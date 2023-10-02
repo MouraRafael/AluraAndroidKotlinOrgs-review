1ad6e84c8795917dc6fd50bfbd83dbf771f717ec
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-02 10:36:56 -0300

    01-14    Faça como eu fiz: Rodando App Android

--- app/src/main/java/br/com/alura/estudo/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/estudo/orgs/MainActivity.kt
@@ -1,7 +1,15 @@
 package br.com.alura.estudo.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity:Activity() {
-
+class MainActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this,"Boas Vindas",Toast.LENGTH_LONG).show()
+    }
 }
