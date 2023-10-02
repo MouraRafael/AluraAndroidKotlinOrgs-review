9fa6bd483eb6bfa79a22a1c9c6a165acdf2967ac
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-02 17:18:08 -0300

    01-04-10        Faça como eu fiz: Implementando os métodos do Adapter

--- app/src/main/java/br/com/alura/estudo/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/estudo/orgs/ui/activity/MainActivity.kt
@@ -3,9 +3,14 @@ package br.com.alura.estudo.orgs.ui.activity
 import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintSet.Layout
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import br.com.alura.estudo.orgs.R
+import br.com.alura.estudo.orgs.model.Produto
 import br.com.alura.estudo.orgs.ui.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity:Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +27,18 @@ class MainActivity:Activity() {
 //        valor.text = "19,99"
 
         val recyclerView:RecyclerView = findViewById(R.id.recycler_view)
-        recyclerView.adapter = ListaProdutosAdapter()
+        recyclerView.adapter = ListaProdutosAdapter(
+            context = this,
+            produtos = listOf(
+                Produto("nome","teste descricao", BigDecimal("9.99")),
+                Produto("nome 3","teste descricao 2",BigDecimal("19.99")),
+                Produto("nome 4","teste descricao 3",BigDecimal("29.99")),
+                Produto("nome 5","teste descricao 4",BigDecimal("39.99")),
+                Produto("nome 6","teste descricao 5",BigDecimal("59.99")),
+            )
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL,false)
 
         //findViewById<TextView>(R.id.nome).setTextSize(TypedValue.COMPLEX_UNIT_SP,25.5f)
 
@@ -37,4 +53,32 @@ class MainActivity:Activity() {
 * Passo 2: Adicionar A RecyclerView no viewGroup  Desejado
 *
 * Passo 3:  Configurar o Componente responsável por fazer a lógica do RecyclerVIew Acontece
+*
+* PASSO 4: Criar uma classe Interna do tipo ViewHolder extendendo de RecyclerView.ViewHolder para pasa substituir a referencia genérica que vem por padrão
+* O ViewHolder recebe uma  View que será que será passado ao construtor da classe mae;
+*
+* Passo 5: Criar a View dentro do OnCreateViewHoldedr
+* usar o LayoutInflater().from() para gerar um layout inflater, exigindo um contexto.
+* Recomenda-se passar esse c ontexto através do do construtor do Adapter
+* A partir do layoutInflater criado , usa-se o metodo inflate() passando como argumentos o resource(o layout que queremos criar)
+* root: que e o viewgroup para onde enviaremos nosso RecyclerView(esse argumento é passado como argumento
+* do onCreateViewHolder), o attach to root, que determinará se anexaremos essa  view diretamente no root
+* ou se isso será feito em outro momento, por padrão se deixa isso como falso no RecyclerView
+* O metodo inflate retornara a view que sera passada ao viewlholder
+*
+*
+* Passo 6: Configuração do binding, pegando cada uma dasa views e colocando o valor esperado
+* No onBindViewHolder, pega-se o item da lista passada com lista[posicao] e o passe como argumento para o método
+* do viewHolder que fara o vinculo, se o metodo nao existir, crie-o
+*
+* Passo 7: vinculo de informacoes
+*   pegar ou criar o metodo de vinculo no viewholder
+*   A referencia ded view da classe viewholder tem e referencia dos elementos através do itemView
+* com o itemView.findViewById
+* e entao e so passar os dados do produto recebido para a view recebida
+*
+*  Passo 8: Na activity,passe o contexto e lista para o recyclerview.adapter
+*
+* Passo 9: Determinar o Layout Manager, isso pode ser feito em codigo usando o recyclerview.LayoutManager
+* ou no xml da Recycler view passando através do app:layoutManager
 * */
--- app/src/main/java/br/com/alura/estudo/orgs/ui/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/estudo/orgs/ui/adapter/ListaProdutosAdapter.kt
@@ -1,19 +1,82 @@
 package br.com.alura.estudo.orgs.ui.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.estudo.orgs.R
 import br.com.alura.estudo.orgs.model.Produto
 
-class ListaProdutosAdapter(private val produtos:List<Produto> ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+class ListaProdutosAdapter(private val context: Context, private val produtos: List<Produto>) :
+    RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun vincula(produto: Produto) {
+            val nome = itemView.findViewById<TextView>(R.id.nome)
+            nome.text = produto.nome;
+
+            itemView.findViewById<TextView>(R.id.descricao).text = produto.descricao
+            itemView.findViewById<TextView>(R.id.valor).text = produto.preco.toPlainString()
+
+        }
+
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater: LayoutInflater = LayoutInflater.from(context)
+        val view: View = inflater.inflate(R.layout.produto_item, parent, false)
+        return ViewHolder(view)
+
     }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val produto = produtos[position]
+        holder.vincula(produto)
+    }
+
     override fun getItemCount(): Int {
         return produtos.size;
     }
 
 }
+/*
+* RecyclerView
+* passo 1:
+* Criar item que será modelo da Recycler view
+*
+* Passo 2: Adicionar A RecyclerView no viewGroup  Desejado
+*
+* Passo 3:  Configurar o Componente responsável por fazer a lógica do RecyclerVIew Acontece
+*
+* PASSO 4: Criar uma classe Interna do tipo ViewHolder extendendo de RecyclerView.ViewHolder para pasa substituir a referencia genérica que vem por padrão
+* O ViewHolder recebe uma  View que será que será passado ao construtor da classe mae;
+*
+* Passo 5: Criar a View dentro do OnCreateViewHoldedr
+* usar o LayoutInflater().from() para gerar um layout inflater, exigindo um contexto.
+* Recomenda-se passar esse c ontexto através do do construtor do Adapter
+* A partir do layoutInflater criado , usa-se o metodo inflate() passando como argumentos o resource(o layout que queremos criar)
+* root: que e o viewgroup para onde enviaremos nosso RecyclerView(esse argumento é passado como argumento
+* do onCreateViewHolder), o attach to root, que determinará se anexaremos essa  view diretamente no root
+* ou se isso será feito em outro momento, por padrão se deixa isso como falso no RecyclerView
+* O metodo inflate retornara a view que sera passada ao viewholder
+*/
+/*
+*
+* Passo 6: Configuração do binding, pegando cada uma dasa views e colocando o valor esperado
+* No onBindViewHolder, pega-se o item da lista passada com lista[posicao] e o passe como argumento para o método
+* do viewHolder que fara o vinculo, se o metodo nao existir, crie-o
+*
+* Passo 7: vinculo de informacoes
+*   pegar ou criar o metodo de vinculo no viewholder
+*   A referencia ded view da classe viewholder tem e referencia dos elementos através do itemView
+* com o itemView.findViewById
+* e entao e so passar os dados do produto recebido para a view recebida
+*
+*  Passo 8: Na activity,passe o contexto e lista para o recyclerview.adapter
+*
+* Passo 9: Determinar o Layout Manager, isso pode ser feito em codigo usando o recyclerview.LayoutManager
+* ou no xml da Recycler view passando através do app:layoutManager
+* */
