25576ffa66c85d6c5ace7999b1e8c6c3419c9621
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-04 16:41:20 -0300

    3-2-05 fix:Dao anotation on ProdutoDAO

--- app/src/main/java/br/com/alura/estudo/orgs/database/dao/ProdutoDao.kt
+++ app/src/main/java/br/com/alura/estudo/orgs/database/dao/ProdutoDao.kt
@@ -1,9 +1,11 @@
 package br.com.alura.estudo.orgs.database.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import br.com.alura.estudo.orgs.model.Produto
 
+@Dao
 interface ProdutoDao {
 
     @Query("SELECT * FROM Produto")
