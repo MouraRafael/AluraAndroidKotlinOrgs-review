89dafcfe42a15d15d2b0429c6f3e998c70acc26a
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-05 13:30:46 -0300

    3-5-03     Faça como eu fiz: Removendo o produto do Room

--- app/src/main/java/br/com/alura/estudo/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/br/com/alura/estudo/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.estudo.orgs.R
+import br.com.alura.estudo.orgs.database.AppDataBase
 import br.com.alura.estudo.orgs.databinding.ActivityProdutosDetalhesBinding
 import br.com.alura.estudo.orgs.model.Produto
 import br.com.alura.estudo.orgs.tentaCarregarImagem
@@ -18,8 +19,10 @@ import java.text.NumberFormat
 import java.util.Locale
 
 private const val TAG = "DetalhesProduto"
-class DetalhesProdutoActivity:AppCompatActivity() {
 
+class DetalhesProdutoActivity : AppCompatActivity() {
+
+    private lateinit var produto: Produto
     private val binding by lazy {
         ActivityProdutosDetalhesBinding.inflate(layoutInflater)
     }
@@ -40,39 +43,49 @@ class DetalhesProdutoActivity:AppCompatActivity() {
 
         setContentView(binding.root)
         //supportActionBar?.hide()
-        val produto:Produto = intent.getParcelableExtra("chave")!!
-
-        val precoFormatado = NumberFormat.getCurrencyInstance(Locale("pt", "br")).format(produto.preco)
-
-        binding.activityDetalhesProdutoValor.text = precoFormatado
-        binding.activityDetalhesProdutoTitulo.text = produto.nome
-        binding.activityDetalhesProdutoDetalhes.text = produto.descricao
-        binding.activityDetalhesProdutoImagem.tentaCarregarImagem(produto.imagem,imageLoader)
+        intent.getParcelableExtra<Produto>("chave")?.let { produtoCarregado: Produto ->
+            produto = produtoCarregado
+            preencheCampos(produtoCarregado)
+        } ?: finish()
 
 
     }
 
+    private fun preencheCampos(produto: Produto) {
+        val precoFormatado =
+            NumberFormat.getCurrencyInstance(Locale("pt", "br")).format(produto.preco)
+
+        binding.activityDetalhesProdutoValor.text = precoFormatado
+        binding.activityDetalhesProdutoTitulo.text = produto.nome
+        binding.activityDetalhesProdutoDetalhes.text = produto.descricao
+        binding.activityDetalhesProdutoImagem.tentaCarregarImagem(produto.imagem, imageLoader)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
         //basicamente, infle o menu com o arquivo de menu e passe para o onCreateOptionsMenu
-        menuInflater.inflate(R.menu.menu_editar,menu)
+        menuInflater.inflate(R.menu.menu_editar, menu)
         return super.onCreateOptionsMenu(menu)
 
     }
 
     //Filtro para quando os itens do menu forem selecionados
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId){
-            R.id.menu_detalhes_produto_editar->{
-                    Log.i(TAG,"onOptionsItemSelected Editar")
-                    true
+        if (::produto.isInitialized) {
+            val dao = AppDataBase.instanciar(this).dao()
+            when (item.itemId) {
+                R.id.menu_detalhes_produto_editar -> {
+                    Log.i(TAG, "onOptionsItemSelected Editar")
                 }
-                R.id.menu_detalhes_produto_excluir->{
-                    Log.i(TAG,"onOptionsItemSelected Excluir")
-                    true
+                R.id.menu_detalhes_produto_excluir -> {
+                    dao.remove(produto)
+                    finish()
+                }
             }
-            else -> super.onOptionsItemSelected(item)
+
         }
 
 
+        return super.onOptionsItemSelected(item)
     }
 }
